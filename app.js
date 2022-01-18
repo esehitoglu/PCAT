f7da7ea0524439c9f26a767bd6eeffe384333924
kodluyoruz <ensarsehitoglu25@gmail.com>
2022-01-18 18:13:05 +0300

    güncel

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const fn = function () {
-  console.log("function working");
-};
+const express = require('express')
+const ejs = require('ejs')
+const app = express()
+const path = require('path')
+
+// template engine
+app.set('view engine','ejs')
+
+// middlewares
+app.use(express.static('public'))
+
+// routes
+app.get('/',(req,res)=>{
+  res.render('index')
+})
+app.get('/about',(req,res)=>{
+  res.render('about')
+})
+app.get('/add',(req,res)=>{
+  res.render('add')
+})
+
+const port = 3000
+app.listen(port,()=>{
+  console.log(`Sunucu ${port} portunda başlatıldı`)
+})
